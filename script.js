f5d0085e999375081fd083ce90d7f88ff42e6159
Kevin Blanchard <blanchardkevinj@gmail.com>
2025-02-11 18:27:29 -0500

    update to add blogs, not finished

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function toggleDarkMode() {
     document.body.classList.toggle("light-mode");
 }
 
+function blog() {
+    fetch('blog.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('blogmode').innerHTML = html;
+        })
+        .catch(error => console.error('Error loading blog:', error));
+}
+
 document.getElementById("year").textContent = new Date().getFullYear();
 
 const typingElement = document.getElementById("typing-text");
@@ -28,6 +37,21 @@ function eraseText() {
         textIndex = (textIndex + 1) % textArray.length;
         // typingElement.textContent = "&nbsp";
         setTimeout(typeText, 500);
+    }
+}
+function toggleView() {
+    let projectsSection = document.getElementById("projects-section");
+    let blogsSection = document.getElementById("blogs-section");
+    let toggleButton = document.getElementById("toggle-button");
+
+    if (projectsSection.style.display === "none") {
+        projectsSection.style.display = "block";
+        blogsSection.style.display = "none";
+        toggleButton.textContent = "Show Blogs";
+    } else {
+        projectsSection.style.display = "none";
+        blogsSection.style.display = "block";
+        toggleButton.textContent = "Show Projects";
     }
 }
 
